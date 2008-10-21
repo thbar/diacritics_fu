85c9981e99a2f9db35c5155a29428f72bd846474
Thibaut Barrère <thibaut.barrere@gmail.com>
2008-10-21 12:10:16 +0200

    Added TODO to README and bit of rdoc

--- lib/diacritics_fu.rb
+++ lib/diacritics_fu.rb
@@ -1,4 +1,10 @@
+# A tiny class to remove accents (éèê) and other diacritics (ç) from a string.
+#
+# Author::    Thibaut Barrère  (mailto:thibaut.barrere@gmail.com)
+# Copyright:: Copyright (c) 2008 LoGeek EURL
+# License::   Distributes under the same terms as Ruby
 module DiacriticsFu
+  # Remove all accents and other diacritics from the passed string (ie: éphémère will return ephemere)
   def self.escape(str)
     ActiveSupport::Multibyte::Handlers::UTF8Handler.normalize(str,:d).split(//u).reject { |e| e.length > 1 }.join
   end
